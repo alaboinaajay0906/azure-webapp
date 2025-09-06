70bd9450398719478e3eb81cfa61a563cc05dde6
Ajay Kumar <aalaboina@ruggedmonitoring.com>
2025-09-06 13:03:53 +0530

    init commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1> Next JS App Live Final demo!</h1>
+      <p>This is a dummy Next.js app deployed with GitHub Actions.</p>
+    </div>
+  );
+}
