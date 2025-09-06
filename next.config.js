81583618d43020ec4be1b1d40e5ff6701fd17456
Ajay Kumar <145137280+alaboinaajay0906@users.noreply.github.com>
2025-09-06 19:12:18 +0530

    updated index.json, next.config.js and deploy.yml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "standalone", // required for Azure App Service
 };
 
 module.exports = nextConfig;
