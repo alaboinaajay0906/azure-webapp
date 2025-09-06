ca4404382e904877e2501b4af44e47c535a49ad1
Ajay Kumar <145137280+alaboinaajay0906@users.noreply.github.com>
2025-09-06 21:19:40 +0530

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1> Next JS App is Live on Azure!</h1>
+      <h1> Next JS App is Live on Azure !!</h1>
       <p>This is a Next.js app deployed with GitHub Actions.</p>
     </div>
   );
